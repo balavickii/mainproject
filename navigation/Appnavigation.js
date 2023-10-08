c48c71619d435812986df1d5c8842eaf3c467b70
balavickii <vbala4055@gmail.com>
2023-10-09 04:18:48 +0530

    final cpy

--- navigation/Appnavigation.js
+++ navigation/Appnavigation.js
@@ -9,19 +9,15 @@ import Lawyer from "../components/Lawyer";
 import Ulogin from '../components/Ulogin';
 import Onboarding from '../components/Onboarding';
 import Userpage from '../components/Userpage';
-<<<<<<< HEAD
 import Child from '../components/slider/Child'
 import Food from '../components/slider/Food'
 import Consumer from '../components/slider/Consumer'
 import Human_rights from '../components/slider/Human_rights'
 import Marriage from '../components/slider/Marriage'
 import Motor_vehicle from '../components/slider/Motor_vehicle'
-import CarouselItem from '../components/carousel_slide/CarouselItem'
-=======
 import Splash from '../components/Splash';
 import { Provider } from 'react-redux';
 import Store from '../Context/Store';
->>>>>>> 9eb779723c89699087da778668fe4cb2c741164e
 
 
 
@@ -32,12 +28,13 @@ const Appnavigation = () => {
    <NavigationContainer>
     <Provider store={Store}>
     <Stack.Navigator screenOptions={{headerShown:false}}>
-<<<<<<< HEAD
+
         <Stack.Screen name='Logo' component={Logo} />
         <Stack.Screen name='Ulhome' component={Ulhome}/>
         <Stack.Screen name='User' component={User}/>
         <Stack.Screen name='Lawyer' component={Lawyer}/>
         <Stack.Screen name="Ulogin" component={Ulogin}/>
+        <Stack.Screen name='Splash' component={Splash}/>
         <Stack.Screen name='Onboarding' component={Onboarding}/>
         <Stack.Screen name='Userpage' component={Userpage}/>
         <Stack.Screen name='Motor_vehicle' component={Motor_vehicle} />
@@ -46,23 +43,10 @@ const Appnavigation = () => {
         <Stack.Screen name='Human_rights' component={Human_rights} />
         <Stack.Screen name='Consumer' component={Consumer} />
         <Stack.Screen name='Marriage' component={Marriage} />
-    </Stack.Navigator>
-=======
-  
-  <Stack.Screen name='Logo' component={Logo} />
-  <Stack.Screen name='Ulhome' component={Ulhome}/>
-
-  <Stack.Screen name='User' component={User}/>
-  <Stack.Screen name='Lawyer' component={Lawyer}/>
-  
-  <Stack.Screen name="Ulogin" component={Ulogin}/>
-  <Stack.Screen name='Splash' component={Splash}/>
-  <Stack.Screen name='Onboarding' component={Onboarding}/>
-  <Stack.Screen name='Userpage' component={Userpage}/>
 </Stack.Navigator>
+
     </Provider>
   
->>>>>>> 9eb779723c89699087da778668fe4cb2c741164e
    </NavigationContainer>
   )
 }
